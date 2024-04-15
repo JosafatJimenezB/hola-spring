b95241a93b4b0c8ad4128aafa4b8853fb9490df8
josafat <josa221001@gmail.com>
2024-04-15 13:05:12 -0600

    add: api

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class Controller {
+
+    @GetMapping("/saludo")
+    public String saludo() {
+        return "saludo";
+    }
+}
